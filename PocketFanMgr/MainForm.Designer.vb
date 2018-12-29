1c608eb619a646f8b5a78ab148c9ac16699abcdb
DongJun Park <pdj5096@gmail.com>
2018-12-29 21:15:00 +0900

    초기 커밋.

---
+++ PocketFanMgr/MainForm.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.PictureBox4)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(1, 1)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Padding = New System.Windows.Forms.Padding(15)
+        Me.Panel1.Size = New System.Drawing.Size(346, 135)
+        Me.Panel1.TabIndex = 0
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.PocketFanMgr.My.Resources.Resources.closeicon
+        Me.PictureBox4.Location = New System.Drawing.Point(323, 1)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(22, 22)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.PocketFanMgr.My.Resources.Resources.fan_turbo
+        Me.PictureBox3.Location = New System.Drawing.Point(235, 25)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(10)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(85, 85)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.PocketFanMgr.My.Resources.Resources.fan_sleep
+        Me.PictureBox2.Location = New System.Drawing.Point(130, 25)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(10)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(85, 85)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.PocketFanMgr.My.Resources.Resources.fan_auto
+        Me.PictureBox1.Location = New System.Drawing.Point(25, 25)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(10)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(85, 85)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(348, 137)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "MainForm"
+        Me.Padding = New System.Windows.Forms.Padding(1)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MainForm"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+End Class
---
+++ PocketFanMgr/MainForm.vb
@@ -0,0 +1,115 @@
+﻿Imports Microsoft.Win32
+
+Public Class MainForm
+    'Icon: Fan by Kevin Kwok from the Noun Project
+
+#Region "Aero 그림자 효과 (Vista이상)"
+
+    Protected Overrides Sub OnHandleCreated(e As EventArgs)
+        CreateDropShadow(Me)
+        MyBase.OnHandleCreated(e)
+    End Sub
+
+#End Region
+
+    Public Sub RestartService()
+        Dim oldProcesses = Process.GetProcessesByName("FanMonitorService")
+
+        For Each p In oldProcesses
+            p.Kill()
+        Next
+
+        Dim cmd As New ProcessStartInfo
+
+        With cmd
+            .FileName = Environment.GetEnvironmentVariable("ComSpec")
+            .CreateNoWindow = True
+            .UseShellExecute = False
+            .Arguments = "/c net start ""Fan Monitor"""
+        End With
+
+        Process.Start(cmd)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Try
+            SetDefault()
+        Catch ex As Exception
+            MsgBox("문제 발생. 권한을 확인하세요", vbCritical)
+            GoTo endtsk
+        End Try
+        MsgBox("적용 완료", vbInformation)
+endtsk:
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Try
+            SetSlowest()
+        Catch ex As Exception
+            MsgBox("문제 발생. 권한을 확인하세요", vbCritical)
+            GoTo endtsk
+        End Try
+        MsgBox("적용 완료", vbInformation)
+endtsk:
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        Try
+            SetFastest()
+        Catch ex As Exception
+            MsgBox("문제 발생. 권한을 확인하세요", vbCritical)
+            GoTo endtsk
+        End Try
+        MsgBox("적용 완료", vbInformation)
+endtsk:
+    End Sub
+
+    't를 보니 강도같음.
+
+    Public Sub SetDefault()
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "margin", 5)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t0", 40)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t1", 60)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t2", 75)
+        RestartService()
+    End Sub
+
+    Public Sub SetFastest()
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "margin", 5)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t0", 99)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t1", 99)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t2", 10)
+        RestartService()
+    End Sub
+
+    Public Sub SetSlowest()
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "margin", 5)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t0", 99)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t1", 99)
+        WriteReg("SYSTEM\CurrentControlSet\Services\wfan0109", "t2", 85)
+        RestartService()
+    End Sub
+
+    Public Sub WriteReg(ByVal subKey As String, ByVal keyName As String, ByVal value As Integer)
+        Dim key = Registry.LocalMachine.CreateSubKey(subKey)
+        key?.SetValue(keyName, value, RegistryValueKind.DWord)
+        key?.Close()
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Opacity = 0
+    End Sub
+
+    Private Sub MainForm_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Refresh()
+        FadeIn(Me, 1)
+    End Sub
+
+    Private Sub MainForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        FadeOut(Me)
+    End Sub
+End Class
